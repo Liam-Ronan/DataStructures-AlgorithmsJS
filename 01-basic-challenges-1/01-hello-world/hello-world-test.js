685b2d7a44df551aadc028cf27fe90991f4cea13
Liam Ronan <liamronan16@gmail.com>
2023-09-27 10:22:04 +0100

    checking git

--- 01-basic-challenges-1/01-hello-world/hello-world-test.js
+++ 01-basic-challenges-1/01-hello-world/hello-world-test.js
@@ -1,6 +1,6 @@
 const helloWorld = require('./hello-world');
 
-test("Returning 'Hello, World!' as a string", () => {
+test("Returning 'Hello, World!' as a strin", () => {
   const result = helloWorld();
   expect(result).toBe('Hello World!');
 });
